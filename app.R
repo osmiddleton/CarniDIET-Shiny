53f7c97eff5dec569a9cd039043ba1649a597092
Owen Middleton <om221@sussex.ac.uk>
2021-03-17 10:58:36 +0000

    Updating app

--- app.R
+++ app.R
@@ -20,7 +20,7 @@
 library(pacman)
 pacman::p_load(shiny, leaflet, sf, sp, tidyverse, shinythemes,
                shinyWidgets, golem,RColorBrewer, scales,
-               lattice, dplyr)
+               lattice, dplyr, shinyjs, DT)
 
 # Loading data -----
 carnidiet <- read.csv("./Data/CarniDIET 1.0.csv") # CarniDIET
@@ -39,9 +39,23 @@ studies <- studies %>% filter(!is.na(decimalLatitude) |
                               !decimalLatitude == 0 |
                               !decimalLongitude == 0)
 
-# Change names of coordinatres
+# Change names of coordinates
 tib <- as_tibble(studies)
 tib$id <- 1:nrow(tib)
+
+
+# CarniDIET summarised
+cd_summary <- carnidiet %>% 
+  dplyr::group_by(familyCarni, scientificNameCarni, lifeStageCarni, sexCarni,
+                  foodType,scientificNamePrey,
+                  country, samplingProtocol,methodQuantification, sourceTitle) %>% 
+  dplyr::summarise(x = length(scientificNameCarni))
+
+#cd_summary <- cd_summary[1:10,]
+
+# Get colour scheme for families:
+fams <- levels(factor(tib$familyCarni))
+colour_scheme <- colorFactor(palette = "Paired", fams)
 
 # CarniDIET Shiny App ----
 
@@ -51,95 +65,181 @@ tib$id <- 1:nrow(tib)
 # shinyApp: Combines the above into an App.
 
 
+
+
+
 shinyApp(
+
+  # User interface ----
+  
+  ui = fluidPage(
+    
+    # UI Theme
+    theme = shinytheme("lumen"),
+    
+    # Title of app
+    tags$head(HTML("<title> CarniDIET 1.0 </title>")),
+    
+    # Navigation bar: Add file name in before .png
+    navbarPage(title = span(img(src = "logosmall.png", style = "margin-top: -14px", height = 65)),
+               
+               # 1. Welcome page ----
+               tabPanel("Welcome",
+                        
+                        HTML('<center><img src="logo.png" width = "400" ></center>'),
+                        
+                        h1("Exploring the diets of the world's wild carnivores", align = "center"),
+                        h1(" ", align = "center"),
+                        
+                        HTML('<center><img src="map.png" width = "600"></center>'),
+                        
+                        h4("The world's carnivores are of major interest for conservation research, creative inpiration, and, often, conflict. One aspect of their ecology that fascinates us is their diets, which can vary to certain degrees across their geographic ranges. For any interested party, CarniDIET aims to make current knowledge of their diets available by digitizing past quantitative studies of their diets for all to access.", align = "center"),
+                        h4("CarniDIET is a work in progress, with the first release (CarniDIET 1.0) now available, consisting of data for 103 species of terrestrial, carnivorous mammal.", align = "center"),
+                        h4(strong("We will continue to build the database and welcome input from all to continue to improve data accuracy and accessibility. See plans for future updates on the 'Details' page."), align = "center"),
+                        tags$img(src = "carnivore-banner.png", align = "center", width = "100%"),
+                        
+                        h6("Left to right: stoat (Mustela erminea), black-backed jackal (Canis simensis), gey wolf (Canis lupus), cheetah (Acinonyx jubatus), lion (Panthera leo)"),
+                        h6(strong("Photo credit: Dr. Christopher J Sandom"))
+                        
+               ),
+               
+               
+               # 2. Exploratory data page ----
+               tabPanel("Explore",
+                        
+                        #h1(strong("CarniDIET"), align = "center"),
+                        #HTML('<center><img src="logo.png" width = "400" ></center>'),
+                        
+                        h1("Explore here", align = "center"),
+                        h3("To begin exploring, click a study site on the map or filter for one, or more, taxonomic family, species, or country first using the side menu...", align = "center"),
+                        
+                        sidebarPanel(
+                          #img(src = "logosmall.png"),
+                          h4(strong("What are you interested in?")),
+                          selectizeGroupUI(
+                            id = "explore_filters",
+                            inline = FALSE,
+                            params = list(
+                              family = list(inputId = "familyCarni", title = "What family?", placeholder = "select"),
+                              species = list(inputId = "scientificNameCarni", title = "What species?", placeholder = "select"),
+                              country = list(inputId = "country", title = "What country?", placeholder = "select")))
+                          # uiOutput("family"),
+                          # uiOutput("species"),
+                          # uiOutput("country"),
+                        ),
+
+                        mainPanel(
+                          
+                          leafletOutput("mymap")
+                          #tableOutput("table")
+                        )
+                        
+                        
+                        
+                        
+               ),
+               
+               
+               # 3. Download page (more information etc) ----
+               tabPanel("Download",
+                        
+                        #HTML('<center><img src="logo.png" width = "400" ></center>'),
+                        
+                        h1("Downloading data", align = "center"),
+                        h3("Hit the 'Download' button on the left to get the full CarniDIET 1.0 dataset." , align = "center"),
+                        h5("Filter menu: If you are only interested in a specific family, species, country, or anything else, user the sidebar menu filter first and then hit download." , align = "center"),
+                        h5("Table output: The data shown in the table below is an overview of the data you will download. The downloaded data will include all data fields." , align = "center"),
+                        h5(strong("Reference and acknolwedgements: Any use of these data should cite the manuscript on the Welcome page, although we acknowledge that the real credit goes to the hard work of field researchers who gathered these data."), align = "center"),
+                        
+                        fluidRow(column(width = 3,
+                                        downloadButton("download", "Download the data"),
+                                 selectizeGroupUI(
+                                 id = "download_filters",
+                                 inline = FALSE,
+                                 params = list(
+                                   family_download = list(inputId = "familyCarni", title = "Select family", placeholder = "select"),
+                                   species_download = list(inputId = "scientificNameCarni", title = "Select species", placeholder = "select"),
+                                   country_download = list(inputId = "country", title = "Select country", placeholder = "select"),
+                                   source_download = list(inputId = "sourceTitle", title = "Select source", placeholder = "select"),
+                                   foodType_download = list(inputId = "foodType", title = "Select food type", placeholder = "select"),
+                                   samplingProtocol_download = list(inputId = "samplingProtocol", title = "Select sempling protocol", placeholder = "select"),
+                                   methodQuantification_download = list(inputId = "methodQuantification", title = "Select method", placeholder = "select"),
+                                   preySpecies_download = list(inputId = "scientificNamePrey", title = "Select prey species", placeholder = "select")
+                                   ))),
+                                 
+                                 column(width = 6,
+                                         dataTableOutput("table_download_display")
+                                        )
+                                 )
+                        
+                        # sidebarPanel(
+                        #   #img(src = "logosmall.png"),
+                        #   selectizeGroupUI(
+                        #     id = "my-filters",
+                        #     inline = FALSE,
+                        #     params = list(
+                        #       family = list(inputId = "familyCarni", title = "Select family", placeholder = "select"),
+                        #       species = list(inputId = "scientificNameCarni", title = "Select species", placeholder = "select"),
+                        #       country = list(inputId = "country", title = "Select country", placeholder = "select"))),
+                        #   # uiOutput("family"),
+                        #   # uiOutput("species"),
+                        #   # uiOutput("country"),
+                        #   downloadButton('download', "Download the data")
+                        # ),
+                        # 
+                        # mainPanel(
+                        #   #leafletOutput("mymap"),
+                        #   #p(),
+                        #   tableOutput("table")
+                        # )
+                        # 
+                        ),
+               
+               
+               
+               
+               # 4. Details page -----
+               tabPanel("Details",
+                        
+                        h1(strong("CarniDIET"), align = "center"),
+                        h1("Exploring the diets of the world's wild carnivores", align = "center"),
+                        
+                                 
+                        )
+    ),   
+
+    
+  ),  
   
   
-  
-ui = fluidPage(
-  
-  # UI Theme
-  #theme = shinytheme("united"),
-  
-  tags$head(HTML("<title> CarniDIET 1.0 </title>")),
-  
-  navbarPage(title = span(img(src = "logosmall.png", style = "margin-top: -14px", height = 65))),
-  
-  # Name of the app
-  #column(3, offset = 5, titlePanel(img(src = "logosmall.png"))),
-  
-  #titlePanel(img(src = "logosmall.png")),
-  
-  # Panel layout names and UI output  
-  
-    sidebarPanel(
-        #img(src = "logosmall.png"),
-        selectizeGroupUI(
-          id = "my-filters",
-          inline = FALSE,
-          params = list(
-            family = list(inputId = "familyCarni", title = "Select family", placeholder = "select"),
-            species = list(inputId = "scientificNameCarni", title = "Select species", placeholder = "select"),
-            country = list(inputId = "country", title = "Select country", placeholder = "select"))),
-        # uiOutput("family"),
-        # uiOutput("species"),
-        # uiOutput("country"),
-        downloadButton('download', "Download the data")
-    ),
-  
-    mainPanel(
-        leafletOutput("mymap"),
-        p(), 
-        tableOutput("table")
-  )
-  
-),
 
 
 server = function(input, output, session) {
 
+  # 1. Explore page ----
   
-  # Create new table as the drop-downs are updated
-  tab.family <- reactive({ 
-    
-    tib %>% 
-      filter(familyCarni %in% input$family) 
-    })
   
-  tab.species <- reactive({ 
-    
-    tib %>% 
-      filter(familyCarni %in% input$family) %>% 
-      filter(scientificNameCarni %in% input$species) 
-  })
-
+  # Filter table following user input for plotting on map:
   res_mod <- callModule(
     module = selectizeGroupServer,
-    id = "my-filters",
+    id = "explore_filters",
     data = tib,
     vars = c("familyCarni", "scientificNameCarni", "country")
   )
   
     
-  # # Drop-down for families
-  # output$family <- renderUI({
-  #           selectizeInput('family', 'Select family', choices = c("select" = "", levels(tib$familyCarni)))
-  # })
-  #   
-  # # Drop-down for species
-  # output$species <- renderUI({
-  #   # Subset by the selected family
-  #   choice_species <- reactive({
-  #     tib %>% filter(familyCarni == input$family) %>% pull(scientificNameCarni) %>% as.character()
-  #   })
-  #           # Drop-down for carnivore in that family
-  #           selectizeInput('species', 'Select species', choices = c("select" = "", choice_species()))
-  # })
-   
-  
-  # Create map to show all data
+  # Map of study locations, including any user filtering:
   output$mymap <- renderLeaflet ({
     leaflet() %>%
-      addTiles() %>%
+      addProviderTiles("Esri.WorldImagery", group = "Satellite") %>% 
+      addTiles(group = "Countries") %>%
+      
+      addLayersControl(
+        position = "bottomright",
+        baseGroups = c("Satellite", "Countries"),
+        options = layersControlOptions(collapsed = FALSE)
+      ) %>% 
+      
       setView(0,20, zoom = 1.5) %>% 
       addCircles(data = tib,
                  lng = ~decimalLongitude,
@@ -147,62 +247,114 @@ server = function(input, output, session) {
                  #popup = ~paste(familyCarni, scientificNameCarni, country, sep = "; "),
                  color = "lightgrey") %>%
 
-      # addCircles(data = tab.family(),
-      #            lng = ~decimalLongitude,
-      #            lat = ~decimalLatitude,
-      #            popup = ~paste(familyCarni, scientificNameCarni, country, sep = "_"),
-      #            color = "Red") %>%
-      # 
       # Update to show just  the species selected
-      addCircles(data = res_mod(),
+      addCircleMarkers(data = res_mod(),
                  lng = ~decimalLongitude,
                  lat = ~decimalLatitude,
+                 color = ~colour_scheme(familyCarni),
+                 radius = 1,
                  popup = ~paste(sep = "<br/>",
                                 paste0("<B>", country ,"</B>"),
                                 paste0("<B>", familyCarni,": </B>", "<I>", gsub("_", " ", scientificNameCarni), "</I>"),
                                 paste0("Life stage: ", lifeStageCarni),
                                 paste0("Sex: ", sexCarni)
+                                
                  ),
-                 #popup = ~ paste(familyCarni, scientificNameCarni, country, sep = "; "),
-                 color = "black",
-                 weight = 5)
-       # addPolygons(data = range(),
-       #          color = "red",
-       #          smoothFactor = 1,
-       #          opacity = 0.5)
-       # 
+                 layerId = ~id,
+                 weight = 5
+                 ) %>% 
+      
+      addLegend('bottomleft', pal = colour_scheme, values = fams,
+                title = 'Family',
+                opacity = 1)
   })
 
-  # Generate a table of the studies for the selected species
-  output$table <- renderTable({
+  
+  # Filter CarniDIET to the study that has been clicked on:
+  
+  # Trying to create an observe click event to filter the dataframe
+  observeEvent(input$Map_marker_click, { # update the map markers and view on map clicks
     
-    res_mod()
+    p <- input$Map_marker_click
+    proxy <- leafletProxy("Map")
+    if(p$id=="Selected"){
+      proxy %>% removeMarker(layerId="Selected")
+    } else {
+      proxy %>% setView(lng=p$lng, lat=p$lat, input$Map_zoom) %>% acm_defaults(p$lng, p$lat)
+    }
+  })
+  
+  # Create a figure to display the diet of that species in this location
+  
+  
+  
+  
+  
+  
+  
+  # Download page ----
+  
+  # The data displayed on this page is a subset of what would be downloaded.
+  # It includes two sections of code:
+  
+  # 1) Filtering the subset of data for display
+  # 2) Downloading the full dataset depending on user filters
+  
+  # Create user input filtering set up (for both display and main CarniDIET database filtering)
+  res_mod_download_display <- callModule(
+    module = selectizeGroupServer,
+    id = "download_filters",
+    data = cd_summary,
+    vars = c("familyCarni",
+             "scientificNameCarni",
+             "country",
+             "sourceTitle",
+             "foodType",
+             "samplingProtocol",
+             "methodQuantification",
+             "scientificNamePrey")
+  )
+  
+  
+  # Create table of simple information for users to view...
+  output$table_download_display <- DT::renderDataTable({
+    
+    data <- res_mod_download_display()
+    
+    DT::datatable(data) %>% formatStyle("sourceTitle","white-space"="nowrap")
     
   })
+  
+  
+  # Filter table for download:
+  res_mod_download <- callModule(
+    module = selectizeGroupServer,
+    id = "download_filters",
+    data = carnidiet,
+    vars = c("familyCarni",
+             "scientificNameCarni",
+             "country",
+             "sourceTitle",
+             "foodType",
+             "samplingProtocol",
+             "methodQuantification",
+             "scientificNamePrey")
+  )
+  
+  
   
   # Allow users to download the data.
   output$download <- downloadHandler(
     
     filename = function() {"diet.csv"},
     content = function(fname) {
-      write.csv(res_mod(), fname)
+      write.csv(res_mod_download(), fname)
     }
     
   )
   
   
-# This apparently can update the map
 
-# observe({
-#   leafletProxy("map", data = tab()) %>%
-#     clearShapes() %>%
-#     addCircleMarkers(lat = ~lat,
-#                      lng = ~long,
-#                      popup = ~CarniBinom)
-# })
-
-}
-
-)
+})
 
 #shinyApp(ui = ui, server = server)
